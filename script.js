acdc1d68b1583c1ada97d586bb9ed7f95dddb570
Prince <prius2055@gmail.com>
2023-03-06 13:42:41 +0100

    Function added; mobile menu disappears while clicking on mobile link

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const hamburger = document.querySelector(".hamburger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
 
 document.querySelectorAll(".mobile-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
