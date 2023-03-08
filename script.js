b562435f8a30f2ca6c064619ad13a12e23260b3c
emhamza <imumairhamza@gmail.com>
2023-03-08 15:19:14 +0500

    Script.js file modified and desktop popup window designed

--- script.js
+++ script.js
@@ -113,64 +113,91 @@ let projects= [
   },
 ];
 
-const allCards = () => {
-  const card = projects.map(
-    (item) => `
-                <div class='card-container'>
-                  <div class='project-card'>
-                    <div class='card-title'><h3>${item.title}</h3></div>
-                    <ul class='tech'>
-                      <li>${item.technologies[0]}</li>
-                      <li>${item.technologies[1]}</li>
-                      <li>${item.technologies[2]}</li>
-                      <li>${item.technologies[3]}</li>
-                    </ul>
-                    <div class="b_div">
-                      <button data-modal-target="#modal-popup" class="see-button">
-                        See Project
-                      </button>
-                    </div>
-                  </div>    
-                </div>
-              `
-  );
-  return card;
-};
-// window.addEventListener('load', () => {
-//   document.getElementsByClassName('card-container').innerHTML = cards().join('');
-// });
-// const popupWindow = document.getElementsByClassName('mobile-popup-model');
-// const closePopup = () => {
-//   popupWindow.innerHTML = '';
-//   popupWindow.style.padding = '0';
-//   document.getElementsByClassName('')
+// append
+// popupfunction
+// const totalElement = document.qurrySelector('#total-element');
+const openPop = document.qurrySelectorAll('.open-popup');
+const popWindow = document.qurrySelectorAll('.mobile-popup-model');
+openPop.forEach((seeBtn, index) => {
+  seeBtn.addEventListener('click', () => {
+    popWindow.style.display = 'flex';
+    totalElement.classList.add('blur');
+    popWindow.innerHTML = `<div class='mobile-popup-model'>
+                            <div class='popup-2'>
+                                <img ${projects.featuredImageM}>
+                                <img id='close-button' ${projects.closeImage}>
+                                <h3 class='popup-name'>${projects.name}</h3>
+                                <ul class='tech'>
+                                  <li>${projects.technologies[0]}</li>
+                                  <li>${projects.technologies[1]}</li>
+                                  <li>${projects.technologies[2]}</li>
+                                </ul>
+                                <P Class='discription'>${projects.discription}</p>
+                                <div class='popup-buttons'>
+                                  <button href='${projects.linkToLiveVersion}'></button>
+                                  <button href='${projects.linkToSource}'></button>
+                                </div>
+                            </div>
+                          </div>
+    `;
+  });
+});
 
+document.body.appendChild(popWindow);
+
+popWindow.addEventListener('click', (e) => {
+  if (e.target.id === 'close-button'){
+    popWindow.style.display = 'none';
+    totalElement.classList.remove('blur');
+  }
+});
+
+const workCards = document.querySelector('.all-work-cards');
+
+
+// closeMobilePopUp.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = button.closest('.mobile-popup-model')
+//     closeModal(modal)
+//   })
+// })
+
+// function openModal(modal) {
+//   if(modal == null) return
+//   modal.classList.add('active')
 // }
-// The mobile popup goes there
 
-const openMobilePopUp = document.querySelectorAll('[data-modal-target]')
-const closeMobilePopUp = document.querySelectorAll('[data-close-button]')
+// function closeModal(modal) {
+//   if(modal == null) return
+//   modal.classList.remove('active')
+// }
+// The desktop popup goes there
 
-openMobilePopUp.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
 
-closeMobilePopUp.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.mobile-popup-model')
-    closeModal(modal)
-  })
-})
 
-function openModal(modal) {
-  if(modal == null) return
-  modal.classList.add('active')
-}
+// const openDesktopPopUp = document.querySelectorAll('[data-modal-target]')
+// const closeDesktopPopUp = document.querySelectorAll('[data-close-button]')
 
-function closeModal(modal) {
-  if(modal == null) return
-  modal.classList.remove('active')
-}
+// openDesktopPopUp.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = document.querySelector(button.dataset.modalTarget)
+//     openModal(modal)
+//   })
+// })
+
+// closeDesktopPopUp.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = button.closest('.desktop-popup-model')
+//     closeModal(modal)
+//   })
+// })
+
+// function openModal(modal) {
+//   if(modal == null) return
+//   modal.classList.add('active')
+// }
+
+// function closeModal(modal) {
+//   if(modal == null) return
+//   modal.classList.remove('active')
+// }
