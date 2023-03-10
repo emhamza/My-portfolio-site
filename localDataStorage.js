9443f1a200d67b6343fe9da432864464bac50b9e
emhamza <imumairhamza@gmail.com>
2023-03-11 00:44:18 +0500

    Local storage functionality added and updated

--- localDataStorage.js
+++ localDataStorage.js
@@ -1,20 +1,20 @@
 const fullName = document.querySelector('#username');
 const feedback = document.querySelector('#lowerCase');
-const Messasge = document.querySelector('#message');
+const Message = document.querySelector('#message');
 
 function visitorData() {
   const visitor = {
     Name: fullName.value,
     Email: feedback.value,
-    Message: message.value,
+    Message: Message.value,
   };
   localStorage.setItem('visitor', JSON.stringify(visitor));
 }
-[fullName, feedback, message].forEach((input) => input.addEventListener('focusout', visitorData));
+
+[fullName, feedback, Message].forEach((input) => input.addEventListener('focusout', visitorData));
 const visitorDataExist = JSON.parse(localStorage.getItem('visitor'));
 if (visitorDataExist) {
   fullName.value = visitorDataExist.Name;
   feedback.value = visitorDataExist.Email;
-  message.value = visitorDataExist.Message;
-
+  Message.value = visitorDataExist.Message;
 }
